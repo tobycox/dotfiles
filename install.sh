e12a17863f37d33d5ecba37210203ab41cd729d9
Toby Cox <toby@tobycox.com>
2015-06-22 11:37:13 +0100

    Add git commit hooks

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s ~/dev/dotfiles/vimrc ~/.vimrc
 ln -s ~/dev/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dev/dotfiles/git_template ~/.git_template
+
+git config --global init.templatedir '~/.git_template'
