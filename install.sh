f58891f78fc474ac8318525da154b9bddcdfc5e6
Toby Cox <toby@Tobys-MacBook-Pro.local>
2015-10-11 16:09:56 +0100

    Add ctags

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/git_template ~/.git_template
+ln -s $PWD/ctags ~/.ctags
 
 ln -s $PWD/muxit /usr/local/bin
 
 git config --global init.templatedir '~/.git_template'
+
+brew install tmux
+brew install ctags-exuberant
