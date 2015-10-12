462fa59852d52dfef011536d55a96e21561a2667
Toby Cox <toby@tobycox.com>
2015-10-12 10:50:57 +0100

    Node and nvim changes

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s $PWD/vimrc ~/.vimrc
+ln -s .vimrc .nvimrc
+ln -s .vim .nvim
+
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/git_template ~/.git_template
 ln -s $PWD/ctags ~/.ctags
+ln -s $PWD/npmrc ~/.npmrc
 
 ln -s $PWD/muxit /usr/local/bin
 
