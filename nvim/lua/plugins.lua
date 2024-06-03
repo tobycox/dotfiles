a12f72f1cbe97e5cfc13a79c61691c6062f65339
Toby Cox <toby@dovetailstudios.com>
2024-06-03 21:15:39 +1200

    Adding coq

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -95,6 +95,9 @@ return {
       vim.g.coq_settings = {
           auto_start = true, -- if you want to start COQ at startup
           -- Your COQ settings here
+          keymap = {
+            jump_to_mark = ''
+          }
       }
     end,
     config = function()
